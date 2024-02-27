bfb6fcecfa9349bf44ff693226489437691bb0dd
AndreyPopov <andreypopu@gmail.com>
2024-02-27 15:30:34 +0100

    First Person Movement

---
+++ Nordex Game/Assets/CameraManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    //public Transform orientation;
+    //public float senX, senY;
+
+    //float xRot, yRot;
+
+    //void Start()
+    //{
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //    Cursor.visible = false;
+    //}
+
+    //void Update()
+    //{
+    //    float mouseX = Input.GetAxisRaw("Mouse X") * senX * Time.deltaTime;
+    //    float mouseY = Input.GetAxisRaw("Mouse Y") * senY * Time.deltaTime;
+
+    //    yRot += mouseX;
+
+    //    xRot -= mouseY;
+    //    xRot = Mathf.Clamp(xRot, -90, 90);
+
+    //    transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+    //    orientation.rotation = Quaternion.Euler(0, yRot, 0);
+    //}
+}
