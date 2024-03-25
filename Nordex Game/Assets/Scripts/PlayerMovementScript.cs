74ff1e046b6facaa75e1f16ca75b46d874771334
Deyvidddd <133654435+Deyvidddd@users.noreply.github.com>
2024-03-25 22:09:32 +0100

    Basics

--- Nordex Game/Assets/Scripts/PlayerMovementScript.cs
+++ Nordex Game/Assets/Scripts/PlayerMovementScript.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementScript : MonoBehaviour
 {
+    private Rigidbody rb;
+
     private float movementSpeed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Store the current vertical velocity
+        float verticalVelocity = rb.velocity.y;
 
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized * movementSpeed * Time.deltaTime;
+        // Calculate the horizontal movement input
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        moveInput = transform.TransformDirection(moveInput);
 
-        transform.Translate(movement);
+        // Apply horizontal movement
+        rb.velocity = new Vector3(moveInput.x * movementSpeed, verticalVelocity, moveInput.z * movementSpeed);
     }
 }
