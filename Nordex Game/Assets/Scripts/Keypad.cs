2bb4de5752381c48498948e9f942e90fc1bdd2fa
AndreyPopov <andreypopu@gmail.com>
2024-05-16 13:43:27 +0200

    Bug fixes

--- Nordex Game/Assets/Scripts/Keypad.cs
+++ Nordex Game/Assets/Scripts/Keypad.cs
@@ -1,10 +1,8 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Keypad : Puzzle
