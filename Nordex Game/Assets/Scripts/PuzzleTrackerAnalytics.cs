4e3f3befc82f090f8b8023b80eb62026a1aadd93
Deyvidddd <133654435+Deyvidddd@users.noreply.github.com>
2024-05-24 18:56:02 +0200

    Update

--- Nordex Game/Assets/Scripts/PuzzleTrackerAnalytics.cs
+++ Nordex Game/Assets/Scripts/PuzzleTrackerAnalytics.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
 using UnityEngine.Analytics;
 using System.Collections.Generic;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
 
 public class PuzzleTrackerAnalytics : MonoBehaviour
 {
     private float startTime;
 
     private string currentPuzzle;
+
+    public static PuzzleTrackerAnalytics instance;
+
+    // Change Environment to "build"
+    void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+        if (!Application.isEditor) 
+        {
+            var options = new InitializationOptions();
+            options.SetEnvironmentName("build");
+            UnityServices.InitializeAsync(options);
+        }
+        else UnityServices.InitializeAsync();
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Call when a player starts a puzzle
     public void AnalyticsStartPuzzle(string puzzleName)
